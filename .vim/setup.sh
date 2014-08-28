b1da445912e6346d96575cc5f1d73cb049498cd2
Giovanni Cavallanti <gcavallanti@gmail.com>
2014-08-28 16:32:44 -0400

    Refactor setup.sh

--- .vim/setup.sh
+++ .vim/setup.sh
@@ -1,4 +1,3 @@
-#! /bin/bash
 # set -x
 
 # Utility functions
@@ -9,24 +8,32 @@ function local_dir() {
 
 function get_git() {
     
-    r=$(local_dir "$1")
-    mv "${HOME}/.vim/tmp_bundle/$r" "${HOME}/.vim/bundle/"
-    if cd "$r"; then git pull -q; cd .. ; else git clone -q "$1"; fi
+    echo "$FUNCNAME $1"
+    ldir=$(local_dir "$1")
+    mv "$tmp_bundle_dir/$ldir" "$bundle_dir/" 2> /dev/null
+    if cd "$ldir"; then git pull ; cd .. ; else git clone "$1"; fi
 }
 
 function get_download() {
-    mv "${HOME}/.vim/tmp_bundle/$r" "${HOME}/.vim/bundle/"
+
+    echo "$FUNCNAME $1"
+    mv "$tmp_bundle_dir/$ldir" "$bundle_dir/" 2> /dev/null
+
     #download
 }
 
-# Move current bundles and start a poor man's mark and sweep
-mv "$HOME/.vim/bundle/$r" "$HOME/.vim/tmp_bundle/"
+bundle_dir="$HOME/.vim/bundle"
+tmp_bundle_dir="$HOME/.vim/tmp_bundle"
+
+# Initiate a poor man's mark and sweep
+[[ -d  "$tmp_bundle_dir" ]] && { echo "$tmp_bundle_dir already exists!" ; exit 1; }
+mv "$bundle_dir" "$tmp_bundle_dir"
 
 # Get pathogen
-mkdir -p "${HOME}/.vim/autoload" "${HOME}/.vim/bundle"
+mkdir -p "${HOME}/.vim/autoload" "$bundle_dir"
 curl -Sso "${HOME}/.vim/autoload/pathogen.vim" "https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim"
 
-cd ~/.vim/bundle
+cd "$bundle_dir"
 
 # Get plugins
 get_git "https://github.com/kien/ctrlp.vim.git"
@@ -43,7 +50,7 @@ get_git "https://github.com/mbbill/undotree.git"
 get_git "https://github.com/tpope/vim-commentary.git"
 # get_git "https://github.com/octol/vim-cpp-enhanced-highlight.git"
 # get_git "https://github.com/hail2u/vim-css3-syntax.git"
-get_get "https://github.com/godlygeek/tabular.git"
+get_git "https://github.com/godlygeek/tabular.git"
 get_git "https://github.com/tpope/vim-fugitive.git"
 # get_git "https://github.com/airblade/vim-gitgutter.git"
 get_git "https://github.com/jamessan/vim-gnupg.git"
@@ -56,4 +63,5 @@ get_git "https://github.com/tpope/vim-unimpaired.git"
 get_git "https://github.com/maxbrunsfeld/vim-yankstack.git"
 get_git "https://github.com/gcavn/vimpos.git"
 
-echo "Bundles downloaded. Check tmp_bundle/"
+echo "Bundles downloaded."
+rmdir "$tmp_bundle_dir" 
