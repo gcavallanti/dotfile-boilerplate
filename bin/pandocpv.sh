4ded08b7d9174f7feafbc41f2ef147de6066f3c4
Giovanni Cavallanti <gcavallanti@phy01.local>
2016-01-30 11:27:41 +0100

    wip

--- bin/pandocpv.sh
+++ bin/pandocpv.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
+# Just pass a markdown filename.  A non-default browser can be specified with
+# the -b switch.  Example:
+#
+#   pandocpv.sh -b 'open -g -a "Google Chrome"' myfile.md
+
+set -e
+
+if [[ $(uname -s) == "Darwin" ]]; then 
+  alias browser="open"
+elif [[ $(uname -s) == "Linux" ]]; then
+  alias browser="xdg-open"
+fi
+
+while [[ $# > 1 ]]
+do
+  key="$1"
+
+  case $key in 
+    -b)
+      alias browser="$2"
+      shift
+      ;;
+    *)
+      ;;
+  esac
+  shift
+done
 
 FILE="$1"
-BROWSER="${2:-open}"
 
 out="/tmp/$(basename $FILE).htm"
-pandoc --from markdown_github -s "$FILE" >"$out"
-open -g "$out"
+pandoc --mathjax --highlight-style=monochrome --toc -s "$FILE" >"$out"
+browser "$out"
